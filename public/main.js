dc6ca32b9a6f2def3d0cf786f61e0da8997c1c62
Nickolay Yegorov <nickolay@yegorov.info>
2012-08-09 02:39:48 +0400

    added automated likification for posts and comments

--- public/main.js
+++ public/main.js
@@ -1,10 +1,26 @@
+
+
 $(document).ready(function() {
+
    $("#show-newpost").click(function() {
       $('.header-up').toggle('slow');
       $('.flag').toggle();
      $('.newpost').toggle('slow');
 
    });
+
+  
+
+  //Replace plain URLs with links
+  $('.postcontent').each(function(){
+    $(this).linkify();
+  });
+
+   $('.commentcontent').each(function(){
+    $(this).linkify();
+  });
+
+
 
    $('.plusminus').click(function(){
       $(this).html(
@@ -22,10 +38,14 @@ $(document).ready(function() {
    $('.commentbody').hide();
    $('.editbody').hide();
 
+
+
    $('.editlink').click(function(){
       $(this).parent().find('.postconent').toggle('slow');
       $(this).parent().find('.editbody').toggle('slow');
    });
+
+
 
    $('.commentlink').click(function(){
       $(this).parent().find('.commentbody').toggle('slow');
